481309197d9a93ec60b4acba201da49f9ecc43ea
Tomcat-Guo <zhiqiang.guo@shopee.com>
2022-07-05 14:23:55 +0000

    Update at 2022-07-05T14:23:55.578740Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,6 +1,23 @@
 package com.bytelegend;
 
 public class Challenge {
+    private static int add(int a, int b){
+        int sum = a + b;
+        return sum;
+    }
+    private static int add(int a, int b, int c){
+        int sum = a + b + c;
+        return sum;
+    }
+    private static int add(int a, int b, int c, int d){
+        int sum = a + b + c + d;
+        return sum;
+    }
+    private static String add(String a, String b, String c){
+        String s = a + b + c;
+        return s;
+    }
+
     public static void main(String[] args) {
         System.out.println("1+2=" + add(1, 2));
         System.out.println("1+2+3=" + add(1, 2, 3));
