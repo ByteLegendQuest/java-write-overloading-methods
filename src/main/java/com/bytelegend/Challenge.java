d732555bf3aba0bf2c678f81cac2efde182cf5a8
Euerrur <1822289459@qq.com>
2022-07-19 09:09:28 +0000

    Update at 2022-07-19T09:09:28.930591Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,10 +1,26 @@
 package com.bytelegend;
 
 public class Challenge {
+     void add(int a,int b){
+        int sum = a+b;
+       System.out.println("1+2="+sum);
+    }
+    void add(int a,int b,int c){
+        int sum = a+b+c;
+        System.out.println("1+2+3="+sum);
+    }
+    void add(int a,int b,int c,int d){
+        int sum = a+b+c+d;
+        System.out.println("1+2+3+4="+sum);
+    }
+    void add(){
+        System.out.println("A+B+C="+"ABC");
+    }
     public static void main(String[] args) {
-        System.out.println("1+2=" + add(1, 2));
-        System.out.println("1+2+3=" + add(1, 2, 3));
-        System.out.println("1+2+3+4=" + add(1, 2, 3, 4));
-        System.out.println("A+B+C=" + add("A", "B", "C"));
+        Challenge challenge = new Challenge();
+        challenge.add(1,2);
+        challenge.add(1,2,3);
+        challenge.add(1,2,3,4);
+        challenge.add();
     }
 }
