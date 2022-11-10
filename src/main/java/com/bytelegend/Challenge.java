8dfe6eb359b4bdba4cf40ec7e88448039bcc6851
fanji-jared <2967557176@qq.com>
2022-11-10 08:55:21 +0000

    Update at 2022-11-10T08:55:21.284542Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -8,19 +8,19 @@ public class Challenge {
         System.out.println("A+B+C=" + add("A", "B", "C"));
     }
 
-    public static String add(int i, int j){
-        return i+j;
+    public static String add(int i, int j) {
+        return i + j;
     }
 
-    public static String add(int i, int j, int k){
-        return i+j+k;
+    public static String add(int i, int j, int k) {
+        return i + j + k;
     }
 
-    public static String add(int i, int j, int k, int l){
-        return i+j+k+l;
+    public static String add(int i, int j, int k, int l) {
+        return i + j + k + l;
     }
 
-    public static String add(String a, String b, String c){
-        return a+b+c;
+    public static String add(String a, String b, String c) {
+        return a + b + c;
     }
 }
